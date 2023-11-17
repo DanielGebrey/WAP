c178e5e2aab106b3dad27c1df5263ebb6f0563c5
DanielGebrey <dani.belay13@gmail.com>
2023-11-17 01:19:04 -0600

    little changes

--- Lab12/app.js
+++ Lab12/app.js
@@ -3,34 +3,21 @@ const express = require('express')
 const app = express();
 const path = require('path');
 
+const userRouter = require('./routes/user-route.js')
+const productRouter = require('./routes/product-route.js')
+
 // app.disable('case sensitive routing');
 app.set('port',process.env.PORT || 3000);
 const port = app.get('port');
 
 app.use(express.urlencoded({extended:true}));
 
-app.get('/user',(req,res)=>{
-    res.status(200).sendFile(path.join(__dirname,'user.html'));
-})
-
-app.post('/user',(req,res)=>{
-    console.log(req.body)
-    res.send('login successfully');
-})
-
 app.get('/',(req,res)=>{
-    res.status(200).sendFile(path.join(__dirname,'slash.html'));
+    res.status(200).sendFile(path.join(__dirname,'./views/slash.html'));
 })
 
 
-app.get('/product',(req,res)=>{
-    res.status(200).sendFile(path.join(__dirname,'/product.html'));
-})
-
-app.post('/product',(req,res)=>{
-    console.log(req.body);
-    res.send('Product Add successfuly')
-})
-
+app.use(userRouter);
+app.use(productRouter);
 
 app.listen(3000,()=>console.log("running on 3000"));
